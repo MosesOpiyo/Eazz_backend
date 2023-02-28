6c3461aaf7740efc15ef6d8798e5aa2faa615888
MosesOpiyo <mosesopiyo54@gmail.com>
2023-02-28 13:08:10 +0300

    Addition of transaxtion records

--- Eazz/settings.py
+++ Eazz/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'Authentication',
+    'Receipts',
+    'TranscationRecords',
     'rest_auth',
 ]
 
---
+++ TranscationRecords/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TranscationrecordsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'TranscationRecords'
