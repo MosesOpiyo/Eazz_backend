560d115efc2cb9cdfe7566e9358c8ca8099293c9
MosesOpiyo <mosesopiyo54@gmail.com>
2023-05-01 23:28:14 +0300

    Auth

--- Authentication/sms.py
+++ Authentication/sms.py
@@ -1,19 +1,15 @@
 from twilio.rest import Client
-from dotenv import load_dotenv
 import os
 
-TWILIO_ACCOUNT_SID = str(os.getenv('TWILIO_ACCOUNT_SID'))
-TWILIO_AUTH_TOKEN = str(os.getenv('TWILIO_AUTH_TOKEN'))
-
 def smsVerification(client_number,client_code):
-    account_sid = "AC65209008165d3586d586276d472b6dc4"
-    auth_token = "fde635b77ac2331654b52138723fa375"
+    account_sid = "AC092a81b764531cae2b1ae5ddcef3bcd9"
+    auth_token = "f6a8d50aae590f7c0b9b3646a5abc1c6"
     client = Client(account_sid,auth_token)
 
     message = client.messages \
                     .create(
                         body=f"Your Eazz Verification code is {client_code}",
-                        from_="+13149474262",
+                        from_="+16206229068",
                         to=client_number
                 )
 
