5b815c1505c55263668bc517e434dd8a2a55f0cd
MosesOpiyo <mosesopiyo54@gmail.com>
2023-05-02 15:55:26 +0300

    env file

--- Authentication/sms.py
+++ Authentication/sms.py
@@ -1,9 +1,10 @@
 from twilio.rest import Client
 import os
+from decouple import config
 
 def smsVerification(client_number,client_code):
-    account_sid = "AC092a81b764531cae2b1ae5ddcef3bcd9"
-    auth_token = "f6a8d50aae590f7c0b9b3646a5abc1c6"
+    account_sid = config('TWILIO_ACCOUNT_SID')
+    auth_token = config('TWILIO_AUTH_TOKEN')
     client = Client(account_sid,auth_token)
 
     message = client.messages \
